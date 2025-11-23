d0323321e126900b1df4be82464068fe74f6c83a
shr8ya-glitch <shr8ya@gmail.com>
2025-11-23 12:17:40 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+n=int(input("enter no.of rows"))
+for i in range(n):
+    for j in range (i+1):
+        print("*", end="")
+    print()
---
+++ main2.py
@@ -0,0 +1,28 @@
+n=int(input("enter no.of rows"))
+if n%2==0:
+    h=int(n/2)
+else:
+     h=int(n/2)+1
+s=h-1
+for i in range(1,h=1):
+     for j in range(s+1)
+     print=("")
+     s=s-1
+     num=1
+    for j in range(2*i-1):
+     print(end=str(num))
+     num=num+1
+    print()
+s=1
+for i in range(1,h=1):
+     for in range (s+1)
+     print=("")
+     s=s+1
+     num=1
+    for j in range(2*i-1):
+     print(end=str(num))
+     num=num+1
+    print()
+
+    
+

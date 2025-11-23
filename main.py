343fd5fbd314c0447f6cd1ac48693d08c90b6415
shr8ya-glitch <shr8ya@gmail.com>
2025-11-23 12:19:46 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-n=int(input("number"))
+n=int(input("Enter the number"))
 s=0
-i=1
-while i<=n:
+for i in range(1,n+1):
     s=s+i
-    i=i+1
-print("sum=",s)
-j=0
-while j<=0:
-    print("forever")
+    print(s)
 
+m=input("Enter name")
+r=('')
+for i in m:      
+    r=i+r
+
+print("original name=",n)
+print("reversed name=",r)
+
+a=int(input("Enter the number"))
+print("formatted:".format(n,1))
+for i in range(n,0,-1):
+    print(i)
+
+base=2
+e=3
+r=base**e
+print(r)
